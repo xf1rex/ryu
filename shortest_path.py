16593c49e203b05cafc9d1ff3c3a4e6be702f354
xf1rex <xfirex87@gmail.com>
2018-06-26 16:38:05 +0200

    Update shortest_path.py

--- shortest_path.py
+++ shortest_path.py
@@ -30,7 +30,7 @@ import copy
 import networkx as nx
 
 
-class SimpleSwitch13(app_manager.RyuApp):
+class ShortestPath13(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
