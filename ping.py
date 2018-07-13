e8694b3dad560c07b0253e566af5845def97ab2e
xf1rex <xfirex87@gmail.com>
2018-07-13 16:13:00 +0200

    Create ping.py

---
+++ ping.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+"""
+put this script in your home directory and run it in the MaxiNet CLI with the following command:
+
+      py execfile ('script.py')
+      
+if it were not in your home directory you could use the following command:
+
+      py execfile ('/ <path> /script.py')
+"""
+
+master1.cmd('ping -c1 10.0.0.2')
+root.cmd('ping -c1 10.0.0.1')
+slave1.cmd('ping -c1 10.0.0.1')
+slave2.cmd('ping -c1 10.0.0.1')
+slave3.cmd('ping -c1 10.0.0.1')
+slave4.cmd('ping -c1 10.0.0.1')
+slave5.cmd('ping -c1 10.0.0.1')
+slave6.cmd('ping -c1 10.0.0.1')
+slave7.cmd('ping -c1 10.0.0.1')
+slave8.cmd('ping -c1 10.0.0.1')
+#slave9.cmd('ping -c1 10.0.0.1')
+#slave10.cmd('ping -c1 10.0.0.1')
+#slave11.cmd('ping -c1 10.0.0.1')
+#slave12.cmd('ping -c1 10.0.0.1')
+#slave13.cmd('ping -c1 10.0.0.1')
+#slave14.cmd('ping -c1 10.0.0.1')
+#slave15.cmd('ping -c1 10.0.0.1')
+#slave16.cmd('ping -c1 10.0.0.1')
